6f048278a66f14ecce4849f4973f49e21a5ace1b
npjabirpkv <npjabirpkv@gmail.com>
2019-09-04 22:27:51 +0800

    Update variables.tf

--- app-service-and-sql-database-var/variables.tf
+++ app-service-and-sql-database-var/variables.tf
@@ -4,7 +4,7 @@ variable "resource-group-name" {
 }
 
 variable "app-service-name" {
-  default = "terraform-app-service"
+  default = "terraform-app-service-jabir"
   description = "The name of the Web App"
 }
 
